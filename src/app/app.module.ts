05720d9778db5a8f4acaabb052d00d429d23cfb0
Rahul Pawar <pawarrahul699@gmail.com>
2021-11-28 10:30:56 +0530

    model demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { AppComponent } from './app.component';   
+import { ModalModule } from 'ngx-bootstrap/modal';  
+  
+import { ModaldemoComponent } from './modaldemo/modaldemo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModaldemoComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  
+    BrowserModule,  
+    ModalModule.forRoot(),  
+    FormsModule,  
+    ReactiveFormsModule, NgbModule, 
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
